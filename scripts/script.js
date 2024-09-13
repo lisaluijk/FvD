dae1583f6e6793e6b5337840e6589d51c0985056
lisaluijk <lisa.luijkm@gmail.com>
2024-09-13 14:17:25 +0200

    bedjd

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 // JavaScript Document
-console.log("hi");
+
+// stap 1: zoek de menu-button op en sla die op in een variabele
+let buttonMenu = document.querySelector("nav button");
+let ulMenu = document.querySelector("nav");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+buttonMenu.onclick = toggleMenu;
+
+// stap 3: voeg in de functie een class toe aan de nav
+function toggleMenu() {
+  ulMenu.classList.toggle("toonMenu");
+}
