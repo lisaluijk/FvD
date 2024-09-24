7a0906b091625dfed93ce4387449203485fb87f9
lisaluijk <lisa.luijkm@gmail.com>
2024-09-24 16:11:48 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,20 @@ function toggleMenu() {
   ulMenu.classList.toggle("toonMenu");
   buttonMenu.classList.toggle("toonMenu");
 }
+
+const nextButton = document.querySelector("section:nth-child(4) button:nth-of-type(2)");
+const prevButton = document.querySelector("section:nth-child(4) button:nth-of-type(1)");
+const theList = document.querySelector("section:nth-child(4) > ul");
+const anEl = document.querySelector("section:nth-child(4) > ul > li");
+const elWidth = anEl.offsetWidth;
+
+function nextEl(){
+	theList.scrollLeft = theList.scrollLeft + elWidth;
+}
+
+function prevEl(){
+	theList.scrollLeft = theList.scrollLeft - elWidth; 
+}
+
+nextButton.onclick = nextEl;
+prevButton.onclick = prevEl;
